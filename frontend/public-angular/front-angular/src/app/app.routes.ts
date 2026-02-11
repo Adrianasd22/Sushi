90ea09208e088899643fc1b52e67dd6ed5efd90b
Ainhoa <ainhoaqg33@gmail.com>
2026-02-11 13:40:14 +0100

    feat(import tokens & routing): añado rutas basicas e importo las variables al scss de angular

--- frontend/public-angular/front-angular/src/app/app.routes.ts
+++ frontend/public-angular/front-angular/src/app/app.routes.ts
@@ -6,8 +6,19 @@ import { MenuPage } from './pages/menu-page/menu-page';
 export const routes: Routes = [
   {
     path: '',
+  },
+  {
+    path: 'menu',
     component: MenuPage,
-
+  },
+  {
+    path: '/login',
+  },
+  {
+    path: '/registration',
+  },
+  {
+    path: '**', //Cualquier ruta que no sea las anteriores
   },
 ];
 
@@ -15,6 +26,4 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'disabled' })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-  
-}
+export class AppRoutingModule {}
