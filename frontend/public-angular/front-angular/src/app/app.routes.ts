2b97f4500cdebdbd6b31a335a3520185cd3d8c35
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-04-27 13:43:30 +0200

    feat(Home page): creacion pagina inicio (falta imagen del fondo)

--- frontend/public-angular/front-angular/src/app/app.routes.ts
+++ frontend/public-angular/front-angular/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { NgModule, signal } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuPage } from './pages/menu-page/menu-page';
+import { HomePage } from './pages/home-page/home-page';
 
 
 export const routes: Routes = [
   {
     path: '',
-    component: MenuPage,
+    component: HomePage,
+  },
+  {
+    path: 'home',
+    component: HomePage,
   },
   {
     path: 'menu',
@@ -21,7 +26,7 @@ export const routes: Routes = [
   {
     path: '**', //Cualquier ruta que no sea las anteriores
     loadComponent: () => import('./pages/error404-page/error404-page').then((m) => m.Error404Page),
-  },
+  }
 ];
 
 @NgModule({
