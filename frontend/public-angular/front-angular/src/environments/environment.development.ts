60116c73bd6408652023ae19e177eb7c9ed08628
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-05-26 18:48:56 +0200

    fix(variables de entorno para desarrollo y produccion)

--- frontend/public-angular/front-angular/src/environments/environment.development.ts
+++ frontend/public-angular/front-angular/src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tu-api.com/api',
-  storageUrl: 'https://localhost:8080/storage/',
+  apiUrl: 'http://localhost:8080/api',
+  storageUrl: 'http://localhost:8080/storage/',
   loginUrl: 'http://localhost:8080/api/login',
-  menuUrl: 'http://localhost:4200/menu',  //Para el login-page.ts
+  menuUrl: 'http://localhost:4200/menu',  //Para el login-page.ts Angular
   dashboardUrl: 'http://localhost:5173',  //Para login-page.ts (redireccion al dashboard en react)
 
 };
