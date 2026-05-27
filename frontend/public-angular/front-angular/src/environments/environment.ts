fa42abd6ec1fbb8df7bc0c09691281907df94777
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-05-28 01:56:01 +0200

    fix(arreglando el duckdns)

--- frontend/public-angular/front-angular/src/environments/environment.ts
+++ frontend/public-angular/front-angular/src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  storageUrl: 'http://localhost:8080/storage/',
-  loginUrl: 'http://localhost:8080/api/login',
-  menuUrl: 'http://localhost:4200/menu',  //Para el login-page.ts
-  dashboardUrl: 'http://localhost:5173',  //Para login-page.ts (redireccion al dashboard en react)
+  apiUrl: 'http://52.23.82.120:8080/api',       //Temporalmente se quedan con http, pero eventualmente se podria poner con https
+  storageUrl: 'http://sushi-imagenes-tfg.s3.us-east-1.amazonaws.com/',
+  loginUrl: 'http://52.23.82.120:8080/api/login',
+  menuUrl: 'http://sushimiyu.duckdns.com/menu',  //Para el login-page.ts
+  dashboardUrl: 'http://sushimiyu.duckdns.com/admin',  //Para login-page.ts (redireccion al dashboard en react)
+  redirectRegister: 'http://sushimiyu.duckdns.com/login',  //Para register-page.ts (redirecciona al login tras registrarse)
+
 };
