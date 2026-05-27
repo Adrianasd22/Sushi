3e65234834bec7e533423716299ad702917a1ba9
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-05-27 15:14:56 +0200

    feat(imagenes en s3)

--- despliegue/main.tf
+++ despliegue/main.tf
@@ -122,6 +122,43 @@ resource "aws_eip" "backend_ip" {
   instance = aws_instance.backend.id
 }
 
+# S3 BUCKET PARA IMÁGENES
+
+resource "aws_s3_bucket" "imagenes" {
+  bucket = "sushi-imagenes-tfg"  
+
+  tags = {
+    Name = "sushi-imagenes-tfg"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "imagenes" {
+  bucket = aws_s3_bucket.imagenes.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "imagenes_public" {
+  bucket = aws_s3_bucket.imagenes.id
+  depends_on = [aws_s3_bucket_public_access_block.imagenes]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.imagenes.arn}/*"
+      }
+    ]
+  })
+}
+
 /* AMI UBUNTU */
 data "aws_ami" "ubuntu" {
   most_recent = true
