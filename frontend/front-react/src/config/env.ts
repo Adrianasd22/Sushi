77bb640a5a310ac2502b6b133a0cb87bd35d89f4
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-05-31 15:25:41 +0200

    fix(imagenes productos dashboard)

--- frontend/front-react/src/config/env.ts
+++ frontend/front-react/src/config/env.ts
@@ -14,7 +14,7 @@ const getStorageUrl = () => {
   if (import.meta.env.DEV) {
     return 'http://localhost:8080/storage/';
   }
-  return `https://sushi-imagenes-tfg.s3.us-east-1.amazonaws.com/productos/`;
+  return `https://sushi-imagenes-tfg.s3.us-east-1.amazonaws.com/products/`;
 };
 
 export const env = {
