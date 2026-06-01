851277616b9ed5c456f1dc4132e0ab33ceb93aea
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2026-06-01 16:06:53 +0200

    fix(fix)

--- backend/src/bootstrap/app.php
+++ backend/src/bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        URL::forceScheme('https');
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
